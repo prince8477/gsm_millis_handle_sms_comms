befe3c435b60f80af01c08720af0a692322f8b1c
prince kuranchie <prince8477@yahoo.co.uk>
2026-06-29 16:54:32 +0100

    Initial commit

---
+++ gsm_millis_handle_sms_comms.ino
@@ -0,0 +1,287 @@
+/*
+
+  Dectiption:
+    monitors gsm communication 
+    
+  LED/LED_BUILTIN= D8
+*/
+#include <SoftwareSerial.h>
+//#include <String.h>
+
+#define D1 5
+#define D2 4
+#define D7 13
+#define D8 15
+#define LED D8
+
+#define OK 1
+#define NOTOK 2
+#define TIMEOUT 3
+#define RST D7
+
+#define RX D2  //Yellow from  gsm TX
+#define TX D1  //GREEN from GSM RX
+#define BAUD_RATE 9600 //115200
+#define A6baud 9600
+
+unsigned long previousTime=0;
+int interval=100;
+
+SoftwareSerial A6board(RX,TX);
+
+void setup() {  
+  // put your setup code here, to run once:
+  pinMode(LED,OUTPUT);   
+  Serial.begin(9600);
+  A6board.begin(9600);
+  delay(1000);
+  
+  //A6command("", "OK", "yy", 1000,1);
+  A6command("ATE0", "OK", "yy", 1000,1);
+  A6command("AT+CLIP=1", "OK", "yy", 1000,1);             //Caller line display enabled
+  A6command("AT+CNMI=1,2,0,0,0", "OK", "yy", 1000,1);     //SMS notification settings
+  Serial.println("System ready");    
+  
+  flash();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  unsigned long currentMillis=millis();
+   if( (currentMillis - previousTime) > interval){
+        previousTime=currentMillis;
+        updateSerial();
+   }
+}
+/***********************************************************************************************************/
+void updateSerial(){           
+  String sline;  
+ 
+  while(Serial.available()){    
+      handleSerial();
+  }
+  
+  while(A6board.available()){      
+     //Serial.println("updateSerial called");
+     //Serial.println("updateSerial called");
+    handleA6() ;
+  }  
+}
+
+/***********************************************************************************************************/
+void handleA6(){
+  String sline;
+    sline="";
+    while (A6board.available()){     
+      sline+=A6board.readString();  
+       //Serial.println(sline);   
+      //sline+=sline;
+    }
+    
+     
+     Serial.print("Serial RCV from GSM module:");
+     Serial.println(sline);             
+ 
+    
+    if(sline.indexOf("+CMT:")>-1){   handleSMS(sline);  } //New SMS
+}
+
+/***********************************************************************************************************/
+void handleSMS(String sline){
+  int count;
+  String lines[4];
+  String smslines[4];
+  String commandLine;
+  String number;
+  String message;
+  int i;
+  
+  count=splitWord(sline,lines,'\n');
+  if(count>0){
+    //Serial.print("count=");   Serial.println(count);
+    commandLine=lines[1];
+    message=lines[2];
+
+    
+    //Serial.println("message=" + message);    
+    //split commandLine
+    if(splitWord(commandLine,smslines,',')>0){ //separates everyting by comma
+      if(splitWord(smslines[0],smslines,' ')>0){ //separates "+CMT: " and rest of op codes
+        //index 0 =+CMT:  and index 1 sent number
+        number=smslines[1];
+        //Serial.println("Number=" + number);        
+      }         
+    }
+
+
+    //digitalWrite(LED_BUILTIN,LOW);    
+    if(message.startsWith("flash")){ 
+      Serial.println("cmnd=flash");
+      //digitalWrite(LED_BUILTIN,HIGH);
+      for(i=0; i<2; i++) { flash(); } 
+    }
+    
+    Serial.println("commandLine=(" + commandLine + ")");
+    Serial.println("Message=(" + message + ")" );
+    Serial.println("Number=(" + number + ")");
+    //Loops through raw SMS notification packet
+    /*
+    for(i=0;i<count;i++){
+      Serial.print(i,DEC); Serial.print(":");  Serial.println(lines[i]);
+    }
+    */
+  }
+}
+
+/***********************************************************************************************************/
+void flash(){
+  for(int i=0;i<4;i++){
+    digitalWrite(LED,HIGH); 
+    delay(500);
+    digitalWrite(LED,LOW); 
+    delay(500);
+  }
+}
+
+/***********************************************************************************************************/
+void handleSerial(){   
+  String sline;    
+  //String lines[10];
+  //int count;
+  sline=Serial.readStringUntil('\n');  
+  
+  if(sline.indexOf('#')>-1 ) {
+    Serial.println("#custom command recieved");
+     
+     if( sline.indexOf("#flash")>-1)    {    flash();      }
+     if( sline.indexOf("#reset_gsm")>-1){    ResetGSM();      }
+     if( sline.indexOf("#restart")>-1)  {    Restart();      }
+
+    //execute_command(sline); 
+  }  else{      
+    //Serial.print("forwarding to A6board.. ");
+    //Serial.println(sline);    
+    A6board.println(sline);
+  }  
+}
+
+/***************************************************************************************************************/
+void Restart(){
+  ESP.restart();
+}
+
+/***************************************************************************************************************/
+void ResetGSM(){
+  digitalWrite(RST,HIGH);
+  delay(2000);
+  digitalWrite(RST,LOW);
+}
+/***************************************************************************************************************/
+byte splitWord(String inputWord,String lines[],char delimeter) {   
+  int len;  //length of string
+  String sword;   //stores sequence of char  
+  int counter=0; //counts number of commas  
+  char ch;
+ 
+  len=inputWord.length()+1;
+  byte bytes[len];
+  inputWord.getBytes(bytes,len);
+  sword=""; 
+  for(int i=0;i<len;i++){       
+    ch=(char)bytes[i];
+    //Serial.println(String(i,DEC) + " ch=" + String(ch) );
+    
+    if( ((char)bytes[i])==delimeter ){//comma code is 44      
+        lines[counter]=sword;     
+        counter++;
+        sword="";     
+    }///       
+    else if(bytes[i]==34){ //Found speech marks. extract following string until next apostrophe is found then set i to x index      
+      for(int x=i+1;x<len;x++){        
+        if(bytes[x] !=34){          
+          sword+=(char)bytes[x]; //if bytes index value is not speech marks then add to sword string
+        }else{//found end speech marks. end of word;                             
+          i=x;        
+          break;       
+        }         
+      }/// end x for loop
+      
+    }else{             
+          sword+=ch;        
+    }
+  }/// ends for i statement
+  lines[counter]=sword;      
+  
+  return (byte)counter;
+}
+
+/***********************************************************************************************************/
+bool A6begin() {
+  A6board.println("AT+CREG?");
+  byte hi = A6waitFor("1,", "5,", 1500);  // 1: registered, home network ; 5: registered, roaming
+  while ( hi != OK) {
+    A6board.println("AT+CREG?");
+    hi = A6waitFor("1,", "5,", 1500);
+  }
+
+  if (A6command("AT&F0", "OK", "yy", 5000, 2) == OK) {   // Reset to factory settings
+    if (A6command("ATE0", "OK", "yy", 5000, 2) == OK) {  // disable Echo
+      if (A6command("AT+CMEE=2", "OK", "yy", 5000, 2) == OK) return OK;  // enable better error messages
+      else return NOTOK;
+    }
+  }
+  return OK;
+}
+
+/***********************************************************************************************************/
+String A6read() {
+  String reply = "";
+  if (A6board.available())  {
+    reply = A6board.readString();
+  }
+  //  Serial.print("Reply: ");
+  //  Serial.println(reply);
+  return reply;
+}
+
+/***********************************************************************************************************/
+byte A6waitFor(String response1, String response2, int timeOut) {
+  unsigned long entry = millis();
+  int count = 0;
+  String reply = A6read();
+  byte retVal = 99;
+  do {
+    reply = A6read();
+    if (reply != "") {
+      Serial.print((millis() - entry));
+      Serial.print(" ms ");
+      Serial.println(reply);
+    }
+  } while ((reply.indexOf(response1) + reply.indexOf(response2) == -2) && millis() - entry < timeOut );
+  if ((millis() - entry) >= timeOut) {
+    retVal = TIMEOUT;
+  } else {
+    if (reply.indexOf(response1) + reply.indexOf(response2) > -2) retVal = OK;
+    else retVal = NOTOK;
+  }
+  //  Serial.print("retVal = ");
+  //  Serial.println(retVal);
+  return retVal;
+}
+
+/***********************************************************************************************************/
+byte A6command(String command, String response1, String response2, int timeOut, int repetitions) {
+  byte returnValue = NOTOK;
+  byte count = 0;
+  while (count < repetitions && returnValue != OK) {
+    A6board.println(command);
+    Serial.print("Command: ");
+    Serial.println(command);
+    if (A6waitFor(response1, response2, timeOut) == OK) {
+      //     Serial.println("OK");
+      returnValue = OK;
+    } else returnValue = NOTOK;
+    count++;
+  }
+  return returnValue;
+}
